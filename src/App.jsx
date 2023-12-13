3b66c7e92d7de2a0207a06b66731b891a71bf845
Shin-sibainu <shincode0712@gmail.com>
2023-12-13 11:26:28 +0900

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import InboxScreen from "./components/InboxScreen";
 import store from "./lib/store";
 
 //npx chromatic --project-token=chpt_988ba981abeeb08 --storybook-build-dir=storybook-static
+//aa
 
 function App() {
   return (
