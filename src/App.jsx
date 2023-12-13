86af4536070852dc29a800a4b7eaccf0febb5fea
Shin-sibainu <shincode0712@gmail.com>
2023-12-13 11:18:46 +0900

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import InboxScreen from "./components/InboxScreen";
 import store from "./lib/store";
 
+//npx chromatic --project-token=chpt_988ba981abeeb08 --storybook-build-dir=storybook-static
+
 function App() {
   return (
     <Provider store={store}>
